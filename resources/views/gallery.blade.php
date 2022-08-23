e6bbfe019a1a31e23dae9214b0143c2c1742d1c8
fransiskalidya <fransiskaly@gmail.com>
2022-08-23 17:50:22 +0700

    finishing

--- resources/views/gallery.blade.php
+++
@@ -1,8 +0,0 @@
-<x-app-layout title="Gallery">
-    <div class="container">
-        <x-card title='Laravel Framework' subtitle='Laravel is my PHP Framework'>
-            Laravel is a web application framework with expressive, elegant syntax. We’ve
-            already laid the foundation — freeing you to create without sweating the small things.
-        </x-card>
-    </div>
-</x-app-layout>
